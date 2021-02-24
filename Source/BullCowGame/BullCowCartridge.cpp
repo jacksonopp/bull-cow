5094f22463ef421fb2e46d8d34ff69f5d80b98c5
Jackson Oppenheim <jacksonop@gmail.com>
2021-02-23 21:46:23 -0600

    generated unreal vscode project

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,12 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine("Welcome to Bull Cows!");
+    PrintLine("Press enter to continue...");
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
-
+    ClearScreen();
+    FString HiddenWord = "unreal";
 }
